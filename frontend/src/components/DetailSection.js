b2dcccee4959bfbc5da0c8670d913e36cbad25ce
rachel <rachelhu09@gmail.com>
2023-06-29 14:41:15 -0700

    Completed styled functional template for adding experiences, hobbies, projects

--- frontend/src/components/DetailSection.js
+++ frontend/src/components/DetailSection.js
@@ -1,24 +1,27 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Jinja from "./Jinja";
+import { Box } from "@mui/material";
 
 // Powered using Jinja template
 
-export default function WorkSection({
+export default function DetailSection({
   position = "Position",
   company = "Company",
   description = "Description",
+  image_link = "",
 }) {
   const [HTML, setHTML] = useState();
   const data = {
     position: position,
     company: company,
     description: description,
+    image_link: image_link,
   };
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.post("/work_section", data);
+        const response = await axios.post("/detail_section", data);
         const { rendered_data } = response.data;
         setHTML(rendered_data);
       } catch (error) {
@@ -26,7 +29,7 @@ export default function WorkSection({
       }
     };
     fetchData();
-  }, []);
+  });
 
-  return Jinja(HTML);
+  return <Box padding={5}>{Jinja(HTML)}</Box>;
 }
