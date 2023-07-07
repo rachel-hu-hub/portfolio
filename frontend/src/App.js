7e4f6ffc51b22d72b7a91b67475e8def24b3e218
rachel <rachelhu09@gmail.com>
2023-07-07 11:22:01 -0700

    Redo build again

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,7 +22,6 @@ export default function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/5000" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/projects" element={<Projects />} />
           </Routes>
